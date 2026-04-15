75a72f4ff59261d30bc5e991adb3e2aa22ae1958
dustij <dev.dusti@gmail.com>
2026-04-15 13:00:31 -0500

    Add Header and Footer components to layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from '@/components/layout/Footer';
+import Header from '@/components/layout/Header';
+
 export default function layout({
   children,
 }: Readonly<{
@@ -6,7 +9,9 @@ export default function layout({
   return (
     <main className="bg-background min-h-screen">
       <div className="bg-my-background mx-auto flex max-h-screen min-h-screen w-full max-w-[393px] flex-col">
-        {children}
+        <Header />
+        <div className="bg-my-background flex-1">{children}</div>
+        <Footer />
       </div>
     </main>
   );

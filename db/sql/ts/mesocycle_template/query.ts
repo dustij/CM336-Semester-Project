102166885d98ebf3a9fec7e416604e77b0a8dcb9
dustij <dev.dusti@gmail.com>
2026-04-29 11:43:13 -0500

    chore(db): remove unused create table SQL constants from query modules

--- db/sql/ts/mesocycle_template/query.ts
+++ db/sql/ts/mesocycle_template/query.ts
@@ -1,24 +1,8 @@
-export const createMesocycleTemplateTable = `
-CREATE TABLE mesocycle_template (
-  template_id INT AUTO_INCREMENT PRIMARY KEY,
-  created_by_user_id INT NULL,
-  title VARCHAR(255) NOT NULL,
-  duration_weeks TINYINT NOT NULL,
-  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
-  CONSTRAINT fk_template_created_by_user FOREIGN KEY (created_by_user_id) REFERENCES users(user_id)
-    ON DELETE SET NULL,
-  CONSTRAINT chk_template_duration_weeks CHECK (
-    duration_weeks BETWEEN 1 AND 12
-  )
-)
-`;
-
 export const insertMesocycleTemplate = `
 INSERT INTO mesocycle_template (created_by_user_id, title, duration_weeks)
 VALUES (?, ?, ?)
 `;
 
-//TODO: make function to count days in template, add column days_per_week and use this function (maybe use CTE?)
 export const selectMesocycleListByUser = `
 SELECT
   template_id AS id,
